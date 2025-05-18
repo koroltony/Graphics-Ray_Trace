d2d072b0663cb49760ec5b871798dcb2c2eff241
TonyKorol1 <tkorol@ucsb.edu>
2025-05-18 00:32:35 -0700

    Improved readability and organization

--- listed_ray_trace_main.py
+++ listed_ray_trace_main.py
@@ -7,15 +7,11 @@ import numpy as np, imageio
 # import the methods needed for abstract classes:
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from typing import Optional
 
 # create aliases for vec3 to represent points and colors:
 
 color = vec3
 point3 = vec3
-
-# test commit again!
 
 # Create Ray Class:
 
@@ -31,7 +27,114 @@ class ray:
 
     def at(self,t):
         return vec3(self.origin.e + t*self.direction.e)
+    
+    
+# Create interval class to keep track of the valid regions where we expect 
+# to have intersections
 
+class interval:
+    
+    def __init__(self,mini=np.inf,maxi=-np.inf):
+        self.mini = mini
+        self.maxi = maxi
+
+    def size(self):
+        return (self.maxi-self.mini)
+    
+    def contains(self,x):
+        return (self.mini <= x <= self.maxi)
+    
+    def surrounds(self,x):
+        return (self.mini < x < self.maxi)
+    
+    # Create default empty and infinite ranges for ease of use
+    
+    @classmethod
+    def empty(cls):
+        return cls(np.inf, -np.inf)
+    
+    @classmethod
+    def universe(cls):
+        return cls(-np.inf, np.inf)
+    
+
+# Create a camera class to instantiate camera variables, shoot rays, and render
+
+class camera:
+    
+    def __init__(self,aspect_ratio=1.0,image_width=100):
+        self.aspect_ratio = aspect_ratio
+        self.image_width = image_width
+        self.image_height = max(1, int(image_width / aspect_ratio))
+
+        self.center = None
+        self.pixel00_loc = None
+        self.pixel_delta_u = None
+        self.pixel_delta_v = None
+        
+    def initialize(self):
+        self.image_height = max(1, int(self.image_width / self.aspect_ratio))
+        self.center = point3([0, 0, 0])
+    
+        # Viewport
+        focal_length = 1.0
+        viewport_height = 2.0
+        viewport_width = viewport_height * (self.image_width / self.image_height)
+    
+        viewport_u = vec3([viewport_width, 0, 0])
+        viewport_v = vec3([0, -viewport_height, 0])
+    
+        self.pixel_delta_u = viewport_u / self.image_width
+        self.pixel_delta_v = viewport_v / self.image_height
+    
+        viewport_upper_left = (
+            self.center
+            - vec3([0, 0, focal_length])
+            - viewport_u / 2
+            - viewport_v / 2
+        )
+
+        self.pixel00_loc = viewport_upper_left + 0.5 * (self.pixel_delta_u + self.pixel_delta_v)
+
+    def render(self, world):
+        self.initialize()
+        
+        image = np.zeros((self.image_height, self.image_width, 3), dtype=np.uint8)
+    
+        for i in range(self.image_height):
+            for j in range(self.image_width):
+                # find the coordinates of the current pixel:
+    
+                pixel_center = self.pixel00_loc + (i * self.pixel_delta_v) + (j * self.pixel_delta_u)
+                ray_direction = pixel_center - self.center
+    
+                # create ray for this pixel:
+    
+                r = ray(self.center,ray_direction)
+    
+                pixel_color = self.ray_color(r,world)
+                image[i,j,:] = create_color(pixel_color)
+
+        imageio.imwrite('output4.png', image)
+        plt.imshow(image)
+    
+    def ray_color(self,ray,world):
+
+        rec = HitRecord
+        # if the ray hits something:
+        if (world.hit(ray,interval(0,np.inf),rec)):
+            return 0.5*(rec.normal+color([1,1,1]))
+        
+        # If the ray doesn't hit anything (misses the spheres) draw a gradient:
+
+        unit_direction = (ray.direction).unit_vector()
+
+        # create gradient:
+
+        a = 0.5*(unit_direction.y + 1.0)
+        return (1.0-a)*color([1,1,1]) + a*color([0.4,0.4,1])
+    
+    
 
 # Create abstract classes to define how hittable objects should be set up:
 
@@ -56,10 +159,14 @@ class HitRecord:
             self.normal = -outwardNormal
 
 class Hittable(ABC):
-    """Abstract base class for hittable objects."""
+    """Abstract base class for hittable objects.
+    
+    This basically just makes sure that all objects that are introduced to the 
+    scene follow the basic guidelines (include a hit function that defines intersections)
+    """
 
     @abstractmethod
-    def hit(self, ray: ray, ray_tmin: float, ray_tmax: float, rec: HitRecord) -> bool:
+    def hit(self, ray: ray, ray_t: interval, rec: HitRecord) -> bool:
 
         pass
 
@@ -70,7 +177,7 @@ class Sphere(Hittable):
         self.center = center
         self.radius = max(0, radius)  # Ensure the radius is non-negative
 
-    def hit(self, ray: ray, ray_tmin: float, ray_tmax: float, rec: HitRecord) -> bool:
+    def hit(self, ray: ray, ray_t: interval, rec: HitRecord) -> bool:
         """
         Check if a ray intersects the sphere.
 
@@ -79,8 +186,8 @@ class Sphere(Hittable):
         # Compute the vector from the ray's origin to the sphere's center
         center_offset = self.center - ray.origin
         # Compute quadratic coefficients
-        a = r.direction.len_squared()
-        h = r.direction.dot(center_offset)
+        a = ray.direction.len_squared()
+        h = ray.direction.dot(center_offset)
         c = center_offset.len_squared() - self.radius*self.radius
 
         discriminant = h * h - a * c
@@ -91,9 +198,9 @@ class Sphere(Hittable):
 
         # Find the nearest root in the acceptable range
         root = (h - sqrtd) / a
-        if root <= ray_tmin or ray_tmax <= root:
+        if not ray_t.surrounds(root):
             root = (h + sqrtd) / a
-            if root <= ray_tmin or ray_tmax <= root:
+            if not ray_t.surrounds(root):
                 return False
 
         # Use the recorded hit to get ray parameters
@@ -118,18 +225,18 @@ class hittableList:
     def add(self, obj):
         self.objects.append(obj)
 
-    def hit(self, ray: ray, ray_tmin: float, ray_tmax: float, rec: HitRecord) -> bool:
+    def hit(self, ray: ray, ray_t: interval, rec: HitRecord) -> bool:
 
         # temporary intersection at infinity to start off
         temp_rec = HitRecord(None, None, float('inf'))
         # hit flag
         hit_anything = False
         # start at the furthest bound of t
-        closest_so_far = ray_tmax
+        closest_so_far = ray_t.maxi
 
         for obj in self.objects:
             # Check if the ray hits the current object within the current closest range.
-            if obj.hit(ray, ray_tmin, closest_so_far, temp_rec):
+            if obj.hit(ray, interval(ray_t.mini, closest_so_far), temp_rec):
                 hit_anything = True
                 closest_so_far = temp_rec.t
                 rec.p = temp_rec.p
@@ -139,97 +246,22 @@ class hittableList:
 
         return hit_anything
 
-# Function for ray color (gradient background):
+# ------------------------------ Main Function for Rendering ------------------
 
-def ray_color(ray,world):
+def main():
+    cam = camera()
+    cam.aspect_ratio = 16.0 / 9.0
+    cam.image_width = 400
 
-    rec = HitRecord
-    # if the ray hits something:
-    if (world.hit(ray,0,np.inf,rec)):
-        return 0.5*(rec.normal+color([1,1,1]))
+    # Set up world (the scene of objects to render)
+    world = hittableList()
+    world.add(Sphere(point3([0, 0, -1]), 0.2))
+    world.add(Sphere(point3([0, -100.5, -1]), 100))
 
-    unit_direction = (ray.direction).unit_vector()
+    cam.render(world)
 
-    # create gradient:
-
-    a = 0.5*(unit_direction.y + 1.0)
-    return (1.0-a)*color([1,1,1]) + a*color([1,0.4,0.5])
-
-
-# set up image environment:
-
-aspect_ratio = 16.0/9.0
-
-image_width = 400
-
-# get image height from aspect ratio
-
-image_height = int(np.floor(image_width/aspect_ratio))
-
-if image_height < 1:
-    image_height = 1
-
-# Create image matrix:
-
-image = np.zeros((image_height, image_width, 3), dtype=np.uint8)
-
-# set up world (the scene of objects to render):
-
-world = hittableList()
-# XYZ center coordinates and radius
-world.add(Sphere(point3([0,0,-1]), 0.5))
-world.add(Sphere(point3([0,-100.5,-1]), 100))
-
-# set up the camera:
-
-focal_length = 1.0
-viewport_height = 2.0
-# use image ratio to set up viewport aspect ratio
-viewport_width = viewport_height * (image_width/image_height)
-camera_center = point3([0,0,0])
-
-# calculate the vectors that point in the x and y directions across the viewport
-
-# x direction vector
-viewport_u = vec3([viewport_width,0,0])
-# y direction vector
-viewport_v = vec3([0,-viewport_height,0])
-
-# calculate the delta vectors in these directions for the image
-# (viewport vector normalized to the coordinates of the image)
-
-pixel_delta_u = viewport_u/image_width
-pixel_delta_v = viewport_v/image_height
-
-# calculate the location of the upper left pixel
-# relative to the viewport, the image is smaller
-
-# (ul: upper left)
-# Relationship: Image_ul = viewport_ul + avg delta
-# viewport_ul is offset from viewport by half its height and width
-
-viewport_ul = camera_center - vec3([0,0,focal_length]) - viewport_u/2 - viewport_v/2
-
-image_ul = viewport_ul + 0.5 * (pixel_delta_u + pixel_delta_v)
-
-# Render the image:
-
-for i in range(image_height):
-    for j in range(image_width):
-        # find the coordinates of the current pixel:
-
-        pixel_center = image_ul + (i * pixel_delta_v) + (j * pixel_delta_u)
-        ray_direction = pixel_center - camera_center
-
-        # create ray for this pixel:
-
-        r = ray(camera_center,ray_direction)
-
-        pixel_color = ray_color(r,world)
-        image[i,j,:] = create_color(pixel_color)
-
-imageio.imwrite('output4.png', image)
-plt.imshow(image)
+if __name__ == "__main__":
+    main()
 
 
 
